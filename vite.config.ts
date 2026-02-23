ba333d568d829809c6160ed543113539275bae5c
Swapnajit Mishra <aasiiishh@gmail.com>
2026-02-23 11:47:53 +0530

    2nd Commit Trial

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
+    // The React and Tailwind plugins are both required for Make, even if we die
     // Tailwind is not being actively used – do not remove themm
     react(),
     tailwindcss(),
